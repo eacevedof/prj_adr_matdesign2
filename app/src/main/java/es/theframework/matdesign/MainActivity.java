f95c011483afdfaf0848967e1a67dcf518492a08
eacevedof <eacevedof@yahoo.es>
2018-04-20 23:38:19 +0200

    Nuevo Proyecto. Ya se ejecuta en el emulador sin cerrarse.

--- app/src/main/java/es/theframework/matdesign/MainActivity.java
+++ app/src/main/java/es/theframework/matdesign/MainActivity.java
@@ -2,53 +2,12 @@ package es.theframework.matdesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    //metodo onCreate: https://youtu.be/poipVVd2jzU?list=PLyvsggKtwbLX06iMtXnRGX5lyjiiMaT2y&t=238
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
-        // La actividad está a punto de hacerse visible.
-    }//onCreate
-
-    //código descargado de: https://mega.nz/#!xQBxEAaZ!MOs69BrKRd9bcVOLA0nnf0Z_Slz4NgTosdlT_Yw_yfo
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
-        // La actividad está a punto de hacerse visible.
-    }//onStart
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
-        // La actividad se ha vuelto visible (ahora se "reanuda").
-    }//onResume
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
-        // Enfocarse en otra actividad  (esta actividad está a punto de ser "detenida").
-    }//onPause
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
-        // La actividad ya no es visible (ahora está "detenida")
-    }//onStop
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
-        // La actividad está a punto de ser destruida.
-    }//onDestroy
-
-}//MainActivity
+    }
+}
