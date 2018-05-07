16ffc515f4f51fb82db130b0cbc2d8c0b46f1476
eacevedof <eacevedof@yahoo.es>
2018-05-07 08:03:15 +0200

    ComponentFile 1.1.0 metodo write_file

--- app/src/main/java/theframework/android/components/ComponentFile.java
+++ app/src/main/java/theframework/android/components/ComponentFile.java
@@ -6,12 +6,16 @@ import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * ComponentFile 1.1.0
@@ -136,7 +140,7 @@ public class ComponentFile {
 
     private void write(String sData,Context oContext) {
         try {
-            OutputStreamWriter oStreamOut = new OutputStreamWriter(oContext.openFileOutput("config.txt", Context.MODE_PRIVATE));
+            OutputStreamWriter oStreamOut = new OutputStreamWriter(oContext.openFileOutput("config.txt", MODE_PRIVATE));
             oStreamOut.write(sData);
             oStreamOut.close();
         }
@@ -144,6 +148,59 @@ public class ComponentFile {
             Log.e("Exception", "File write failed: " + e.toString());
         }
     }//write
+
+    public void write_file(String sData)
+    {
+
+        try {
+            // catches IOException below
+            final String TESTSTRING = new String("Hello Android");
+
+            /* We have to use the openFileOutput()-method
+             * the ActivityContext provides, to
+             * protect your file from others and
+             * This is done for security-reasons.
+             * We chose MODE_WORLD_READABLE, because
+             *  we have nothing to hide in our file */
+            FileOutputStream fOut = oContext.openFileOutput("samplefile.txt",
+                    MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+
+            // Write the string to the file
+            osw.write(TESTSTRING);
+
+            /* ensure that everything is
+             * really written out and close */
+            osw.flush();
+            osw.close();
+
+            //Reading the file back...
+
+            /* We have to use the openFileInput()-method
+             * the ActivityContext provides.
+             * Again for security reasons with
+             * openFileInput(...) */
+
+            FileInputStream fIn = oContext.openFileInput("samplefile.txt");
+            InputStreamReader isr = new InputStreamReader(fIn);
+
+            /* Prepare a char-Array that will
+             * hold the chars we read back in. */
+            char[] inputBuffer = new char[TESTSTRING.length()];
+            // Fill the Buffer with data from the file
+            isr.read(inputBuffer);
+            // Transform the chars to a String
+            String readString = new String(inputBuffer);
+
+            // Check if we read back the same chars that we had written out
+            boolean isTheSame = TESTSTRING.equals(readString);
+            Log.i("File Reading stuff", "success = " + isTheSame);
+        }
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+    }//write_file
     
     private String read(Context oContext) {
 
