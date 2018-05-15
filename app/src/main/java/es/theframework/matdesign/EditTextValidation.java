a7074624f70a8bf62f4c318550ff57f3e8e0845b
eacevedof <eacevedof@yahoo.es>
2018-05-15 10:33:00 +0200

    leccion 22 - edittext validation

---
+++ app/src/main/java/es/theframework/matdesign/EditTextValidation.java
@@ -0,0 +1,13 @@
+package es.theframework.matdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditTextValidation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text_validation);
+    }
+}
